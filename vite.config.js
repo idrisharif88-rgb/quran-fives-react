05b86f2f4b2f48cc9004f5d224faed10cb79f267
Idris <idrisharif88@gmail.com>
2026-03-25 23:14:08 +0300

    2

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/quran-fives-react/', // 👈 This must match your GitHub repository name
   plugins: [react()],
 })
